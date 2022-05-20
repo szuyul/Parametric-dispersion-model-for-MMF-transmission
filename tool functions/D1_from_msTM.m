944467586bb729caddeff85ac727c511b8aac27b
szu-yu <szuyul@mit.edu>
2022-05-20 18:18:00 -0400

    pull

--- tool functions/D1_from_msTM.m
+++ tool functions/D1_from_msTM.m
@@ -1,4 +1,4 @@
-function D = D_from_msTM(Mn)
+function D1 = D1_from_msTM(Mn)
 % D_from_msTM calculates the D matrix from a msTM with constant spectral sampling rate 
 % 
 % output:
@@ -8,16 +8,16 @@ function D = D_from_msTM(Mn)
 %   D is the linear dispersion matrix
 %
 
-n_f = size(Mn,3);
+Nw = size(Mn,3);
 
 temp1 = zeros(size(Mn,1), 'like', 1+1i);
 temp2 = zeros(size(Mn,1), 'like', 1+1i);
-for ll = 1:n_f-1
+for ll = 1:Nw-1
     temp1 = temp1 + Mn(:,:,ll+1)*Mn(:,:,ll)';
     temp2 = temp2 + Mn(:,:,ll)*Mn(:,:,ll)';
 end
 temp = temp1/temp2;
 
-D = sqrtm(temp*temp')\temp; % force D to be unitary
+D1 = sqrtm(temp*temp')\temp; % force D to be unitary
 
 end
